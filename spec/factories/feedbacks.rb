1c8dd8c41213de6cbe704e9486b5ed35737b8371
Oleg Babiy <olegbabiy.ob@gmail.com>
2017-05-01 16:53:08 +0300

    Change rspec for attachment

--- spec/factories/feedbacks.rb
+++ spec/factories/feedbacks.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
   	age {Faker::Number.between(17,65)}
     name {"Roman Kozak"}
     date {"2017-08-22"}
-    attachment_file_name { Faker::Name.name }
+    attachment_file_name { Faker::File.file_name('foo/bar', 'baz', 'doc') }
   end
 end
