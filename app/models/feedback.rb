de98e613db11ede65cbcf766a6891787cca9847b
Oleg Babiy <olegbabiy.ob@gmail.com>
2017-05-01 19:31:23 +0300

    Attachment file type

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,6 +3,6 @@ class Feedback < ApplicationRecord
 	validates_format_of :name, presence: true, :with => /([A-Z]+[a-z]{2,}\s[A-Z]+[a-z]{2,}$)/, :message => "Name must be in the following format: Name Surname"
 	validates :age, presence: true, inclusion: { in: 17..65 }
 	validates_format_of :date, presence: true, :with => /\d{4}\-\d{2}\-\d{2}/, :message => "Date must be in the following format: yyyy-mm-dd"
-	validates_attachment_content_type :attachment, :content_type => 'text/plain'
-	# validates :attachment, attachment_content_type: { content_type: ["application/pdf", 'application/msword','applicationvnd.ms-word','applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document', 'text/plain']}
+	# validates_attachment_content_type :attachment, :content_type => 'text/plain'
+	validates :attachment, attachment_content_type: { content_type: ["application/pdf", 'application/msword','applicationvnd.ms-word','applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document', 'text/plain']}
 end
