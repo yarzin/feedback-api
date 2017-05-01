f4f40e8a10ab8327e2db49ebd0aae8be0cf7583b
Oleg Babiy <olegbabiy.ob@gmail.com>
2017-05-01 19:28:50 +0300

    Attach file type

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,4 +4,5 @@ class Feedback < ApplicationRecord
 	validates :age, presence: true, inclusion: { in: 17..65 }
 	validates_format_of :date, presence: true, :with => /\d{4}\-\d{2}\-\d{2}/, :message => "Date must be in the following format: yyyy-mm-dd"
 	validates_attachment_content_type :attachment, :content_type => 'text/plain'
+	# validates :attachment, attachment_content_type: { content_type: ["application/pdf", 'application/msword','applicationvnd.ms-word','applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document', 'text/plain']}
 end
